3501aa37cfa688305da774150c8371bb230b74f2
Moacir Gama <lealdev.py@gmail.com>
2025-02-23 21:04:38 +0000

    Config prettier

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
-    return (
-        <div>
-            HomeTeste
-        </div>
-    )
+  return <div>HomeTeste</div>;
 }
+
+const newConst = () => {
+  console.log("teste");
+
+  return <div>teste</div>;
+};

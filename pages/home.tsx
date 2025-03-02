e0f98183405cfa22f3a10a919464eff4664a3696
Moacir Gama <lealdev.py@gmail.com>
2025-03-02 16:02:05 +0000

    feat: Create src folder

--- pages/home.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return <div>HomeTeste</div>;
-}
-
-const newConst = () => {
-  console.log("teste");
-
-  return <div>teste</div>;
-};
---
+++ src/pages/home.tsx
@@ -0,0 +1,4 @@
+export default function Home() {
+  console.log("teste");
+  return <div>Runner</div>;
+};

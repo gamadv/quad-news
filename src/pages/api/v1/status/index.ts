e365b358a63ef6a4ffc5ad26ed849eac7ee0eed9
Moacir Gama <lealdev.py@gmail.com>
2025-03-03 19:54:50 +0000

    refactor: Create real data collection to show DB status - plus: update tests

--- src/pages/api/v1/status/index.ts
+++ src/pages/api/v1/status/index.ts
@@ -1,10 +1,39 @@
-// create a next api route function that return a json object with the status of the server
 import { NextApiRequest, NextApiResponse } from "next";
+import { query } from "src/infra/database";
+import { POSTGRES_DB } from "src/infra/envConfig";
 
-export default function Status(req: NextApiRequest, res: NextApiResponse) {
+const databaseName = POSTGRES_DB;
+
+export default async function Status(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const currentTime = new Date().toISOString();
+
+  const getPostgresVersion = await query({
+    queryTextOrConfig: "SHOW server_version;",
+  });
+  const getMaxConnections = await query({
+    queryTextOrConfig: "SHOW max_connections;",
+  });
+  const getCurrentConnections = await query({
+    queryTextOrConfig:
+      "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const postgresVersionResult = getPostgresVersion.rows[0].server_version;
+  const postgresMaxConnections = parseInt(
+    getMaxConnections.rows[0].max_connections,
+  );
+  const postgresCurrentConnections = getCurrentConnections.rows[0].count;
+
   res.status(200).json({
-    status: "ok",
-    message: "Server is running",
-    timestamp: new Date().toISOString(),
+    timestamp: currentTime,
+    db: {
+      version: postgresVersionResult,
+      max_connections: postgresMaxConnections,
+      current_connections: postgresCurrentConnections,
+    },
   });
 }
--- src/tests/integrations/api/v1/status/getStatus.test.ts
+++ src/tests/integrations/api/v1/status/getStatus.test.ts
@@ -1,11 +1,29 @@
-// create an fake test
 describe("Status", () => {
+  const pgBaseVersion = "16.0";
+
   it("GET status should be return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
-    const data = await response.json();
-
-    console.log("🚀 ~ dataRetrieved:", data);
-
     expect(response.status).toBe(200);
+  });
+
+  it("GET status should be return postgresVersion", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const data = await response.json();
+    const pgVersion = data.db.version;
+    expect(pgVersion).toBe(pgBaseVersion);
+  });
+
+  it("GET status should be return maxConnections", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const data = await response.json();
+    const maxConnections = data.db.max_connections;
+    expect(maxConnections).toBeGreaterThanOrEqual(100);
+  });
+
+  it("GET status should be return currentConnections", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const data = await response.json();
+    const currentConnections = data.db.current_connections;
+    expect(currentConnections).toEqual(1);
   });
 });

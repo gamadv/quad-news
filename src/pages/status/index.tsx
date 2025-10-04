4df574f63e9d47f308f656afcac014407a389fb1
gamadv <lealdev.py@gmail.com>
2025-10-04 14:47:40 -0300

    feat: initiate status page at front consuming `api/v1/status` from backend

---
+++ src/pages/status/index.tsx
@@ -0,0 +1,11 @@
+import { SystemInfo } from "./SystemInfo";
+
+export default function StatusPage() {
+  return (
+    <div>
+      <h1>System Info</h1>
+      <p>This is the status page of the application.</p>
+      <SystemInfo />
+    </div>
+  );
+}
